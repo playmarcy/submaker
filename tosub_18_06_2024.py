d3cc1b0931c965549e2a7b20101d36fe4b2ba52a
Dima Chazov <playmarcy@yandex.ru>
2024-06-18 12:41:27 +0300

    nice try

--- tosub_18_06_2024.py
+++ tosub_18_06_2024.py
@@ -22,24 +22,44 @@ with open(f"{file}.txt", "r", encoding="utf-8") as text, open(
 ) as subs:
     subs.write(sub_data)
     text = text.read().replace("\n\n", "\n").split("\n")
-    for numb, string in enumerate(text):SW_EPS_24_ASR
-        if numb % 2 == 0:
+    subs_array = []  # пустой массив для сабов
+    starttime_reserve = None  # зарезервированное время начала
+
+    for numb, string in enumerate(text[::-1]):
+
+        if numb % 2 == 1:
             time = re.findall(r":(\d\d:\d\d:\d\d)]", string)
             starttime = time[0]
             dot = starttime.rfind(":")
             starttime = starttime[:dot] + "." + starttime[dot + 1 :]
+
             try:
                 endtime = time[1]
+                if endtime == "00:00:00":
+                    raise ValueError
                 dot = endtime.rfind(":")
                 endtime = endtime[:dot] + "." + endtime[dot + 1 :]
             except:
+                if starttime_reserve:
+                    endtime = starttime_reserve
+                else:
+                    endtime = starttime
+
+            if endtime == "00:00:00" and starttime_reserve:
+                endtime = starttime_reserve
+            else:
                 endtime = starttime
+
             name = re.findall(r"].*](.+?)$", string)
-        if numb % 2 == 1:
-            text = string
-            if endtime == "00:00:00":
-                endtime = starttime
-            subs.write(
+                
+            subs_array.append(
                 f"Dialogue: 0,0:{starttime},0:{endtime},Default,{name[0]},0,0,0,,{text}\n"
             )
+            starttime_reserve = starttime  # для endtime == "00:00:00"
+
+        if numb % 2 == 0:
+            text = string
+
+    for string in subs_array[::-1]:
+        subs.write(string)
     print("done")

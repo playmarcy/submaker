a158f36cfc94c0f182d432c2b0060f5d21c87c5c
Dima Chazov <playmarcy@yandex.ru>
2024-06-26 12:23:17 +0300

    bla-bla

--- tosub_26_06_2024.py
+++ tosub_26_06_2024.py
@@ -15,52 +15,39 @@ Style: Main_Flashback_Top,Tahoma,22,&H00FFFFFF,&H000000FF,&H00500000,&H96500000,
 [Events]
 Format: Layer, Start, End, Style, Name, MarginL, MarginR, MarginV, Effect, Text\n"""
 
-file = input("Имя файла: ")
 
-with open(f"{file}.txt", "r", encoding="utf-8") as text, open(
-    f"{file}.ass", "w", encoding="utf-8"
+with open(f"text.txt", "r", encoding="utf-8") as text, open(
+    f"text.ass", "w", encoding="utf-8"
 ) as subs:
     subs.write(sub_data)
-    text = text.read().replace("\n\n", "\n").split("\n")
-    text = text[::-1]
+    text = text.read().split("\n\n")
     subs_array = []  # пустой массив для сабов
     starttime_reserve = None  # зарезервированное время начала
 
     for numb, string in enumerate(text):
-        print(string)
-        if numb % 2 == 1:
-            time = re.findall(r":(\d\d:\d\d:\d\d)]", string)
-            starttime = time[0]
+        
+        if numb % 2 == 0:
+            string_array = string.split()
+            starttime = string_array[1]
             dot = starttime.rfind(":")
-            starttime = starttime[:dot] + "." + starttime[dot + 1 :]
-            try:
-                endtime = time[1]
-                if endtime == "00:00:00":
-                    raise ValueError
-                dot = endtime.rfind(":")
-                endtime = endtime[:dot] + "." + endtime[dot + 1 :]
-            except:
-                endtime = "00:00:00"
-                # if starttime_reserve:
-                #     endtime = starttime_reserve
-                # else:
-                #     endtime = starttime
-            if endtime == "00:00:00" and starttime_reserve:
-                endtime = starttime_reserve
-            else:
-                endtime = starttime
+            starttime = starttime[1:dot] + "." + starttime[dot + 1 :]
 
-            name = re.findall(r"]?.*](.+?)$", string)
+            endtime = string_array[2]
+            dot = endtime.rfind(":")
+            endtime = endtime[1:dot] + "." + endtime[dot + 1 :]
+            
+            name = "Default"
+                
+            
 
+        if numb % 2 == 1:
+            text = string.replace("\n", " ")
             subs_array.append(
-                f"Dialogue: 0,0:{starttime},0:{endtime},Default,{name[0]},0,0,0,,{text}\n"
+                f"Dialogue: 0,{starttime},{endtime},Default,{name},0,0,0,,{text}\n"
             )
             starttime_reserve = starttime  # для endtime == "00:00:00"
 
-        if numb % 2 == 0:
-            text = string
-
-    for string in subs_array[::-1]:
+    for string in subs_array:
         subs.write(string)
 
     print("done")
